346ef5000316c5e7277dc3ae1c9260333f6d9426
ruby-live <ruby.childs1@gmail.com>
2016-01-19 11:22:29 +0000

    Setting config file for prod, staging etc

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
-
+import os
 
 app = Flask(__name__)
 
-app.config.from_object('config')
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 from app import views
--- config.py
+++ config.py
@@ -1,7 +1,28 @@
 import os
 
 
-SECRET_KEY = os.urandom(24)
-SESSION_TYPE = 'filesystem'
-MAILCHIMP_API_KEY = os.environ['MAILCHIMP_API_KEY']
-EMAIL_SUBSCRIBER_LIST = os.environ['EMAIL_SUBSCRIBER_LIST']
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.urandom(24)
+    MAILCHIMP_API_KEY = os.environ['MAILCHIMP_API_KEY']
+    EMAIL_SUBSCRIBER_LIST = os.environ['EMAIL_SUBSCRIBER_LIST']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
