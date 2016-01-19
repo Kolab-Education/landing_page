47ec8ed402b583eeec34699d113f78e55dafb923
ruby-live <ruby.childs1@gmail.com>
2016-01-19 11:46:40 +0000

    correcting config

--- config.py
+++ config.py
@@ -6,22 +6,26 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.urandom(24)
-    MAILCHIMP_API_KEY = os.environ['MAILCHIMP_API_KEY']
-    EMAIL_SUBSCRIBER_LIST = os.environ['EMAIL_SUBSCRIBER_LIST']
 
 
 class ProductionConfig(Config):
     DEBUG = False
+    MAILCHIMP_API_KEY = os.environ['MAILCHIMP_API_KEY']
+    EMAIL_SUBSCRIBER_LIST = os.environ['EMAIL_SUBSCRIBER_LIST']
 
 
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    MAILCHIMP_API_KEY = os.environ['MAILCHIMP_API_KEY']
+    EMAIL_SUBSCRIBER_LIST = os.environ['EMAIL_SUBSCRIBER_LIST']
 
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    MAILCHIMP_API_KEY = os.environ['MAILCHIMP_API_KEY']
+    EMAIL_SUBSCRIBER_LIST = os.environ['EMAIL_SUBSCRIBER_LIST']
 
 
 class TestingConfig(Config):
