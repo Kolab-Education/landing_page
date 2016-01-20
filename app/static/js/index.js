9e6178330a9f5958443ae38cc830f9897ee4b1a5
ruby-live <ruby.childs1@gmail.com>
2016-01-20 15:52:32 +0000

    reodering and restyling

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -64,12 +64,12 @@ $('.navbar-collapse ul li a').click(function() {
 });
 
 
-    $(document).ready(function(){
-      $(window).scroll(function() { // check if scroll event happened
-        if ($(document).scrollTop() > 600) { // check if user scrolled more than 50 from top of the browser window
-          $(".navbar").css("background-color", "#c44df1"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-        } else {
-          $(".navbar").css("background-color", "transparent"); // if not, change it back to transparent
-        }
-      });
-    });
+$(document).ready(function(){
+  $(window).scroll(function() { // check if scroll event happened
+    if ($(document).scrollTop() > 600) { // check if user scrolled more than 50 from top of the browser window
+        $(".navbar").css("background-color", "#c44df1"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+    } else {
+        $(".navbar").css("background-color", "transparent"); // if not, change it back to transparent
+    }
+  });
+});
